958065688719dae51dc3bfae6a1c4b751a49ae7f
Harika Vavilapalli <you@example.com>
2025-02-08 12:14:41 +0530

    added seperation of concerns in models and created owner model

---
+++ models/owner-model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+
+
+const userSchema = mongoose.Schema({
+    fullname: {
+        type: String,
+        minLength: 3,
+        trim: true,
+    },
+    email: String,
+    password: String,
+    products:{
+        type: Array,
+        default: [],
+    },
+    picture: String,
+    gstin: String,
+
+});
+
+module.exports = mongoose.model("owner",ownerSchema);
